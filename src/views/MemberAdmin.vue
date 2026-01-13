4ce66664bf0295205080a7857adbd46b0c3e6b98
wei-yan-chen <h153123321@gmail.com>
2026-01-13 10:08:39 +0800

    Delete:刪除站存檔

--- src/views/MemberAdmin.vue
+++ src/views/MemberAdmin.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-import Datatables from '@/components/Datatables.vue';
+import Datatables from '@/components/Datatables.vue'
 
 
 </script>
@@ -9,7 +9,6 @@ import Datatables from '@/components/Datatables.vue';
   <div class="test">
     <h2 class="zh-h2 content-title">會員管理</h2>
     <Datatables/>
-    
   </div>
 </template>
 
